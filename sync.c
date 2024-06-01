70ec6567eb95d3b2468274e5dcb673c3e863c163
Ariel Leyva <aleyva1509@gmail.com>
2024-05-31 20:31:58 -0600

    hotfix, semicolon

--- sync.c
+++ sync.c
@@ -4,8 +4,8 @@ struct FileInfo {
     char filename[256]; // nombre completo del archivo 
     long size; // tamaño del archivo en bytes
     time_t last_modified; // fecha de la última modificación
-}
+};
 
 int main() {
-
+    
 }
