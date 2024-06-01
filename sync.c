b24a71e257d1be2de6d0c72882d4bc27db2506b5
Ariel Leyva <aleyva1509@gmail.com>
2024-05-31 20:31:38 -0600

    add: FileInfo struct

---
+++ sync.c
@@ -0,0 +1,11 @@
+#include <time.h>
+
+struct FileInfo {
+    char filename[256]; // nombre completo del archivo 
+    long size; // tamaño del archivo en bytes
+    time_t last_modified; // fecha de la última modificación
+}
+
+int main() {
+
+}
